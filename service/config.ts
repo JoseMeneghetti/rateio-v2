86a821b1d160f2570a2107f7580ffc025e32db1c
j.pinto <jrmp.dev@gmail.com>
2023-12-06 22:25:13 -0300

    API URL

--- service/config.ts
+++ service/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.API_URL,
 });
 
 // Alter defaults after instance has been created
