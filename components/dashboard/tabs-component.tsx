42de62e2ab900652d536b2b234be0b0cdef70bad
j.pinto <jrmp.dev@gmail.com>
2024-01-05 18:17:05 -0300

    tab icons fix

--- components/dashboard/tabs-component.tsx
+++ components/dashboard/tabs-component.tsx
@@ -3,6 +3,7 @@ import DashboardTabParticipants from "@/components/dashboard/dashboard-tab-parti
 import DashboardTabSuggestions from "@/components/dashboard/dashboard-tab-suggestions";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { IFetchedRateio, IRateio } from "@/store/rateios/rateios.reducer";
+import { BarChartBigIcon, FileHeartIcon, UsersIcon } from "lucide-react";
 
 interface TabsComponentProps {
   rateio: IFetchedRateio | IRateio;
@@ -11,10 +12,25 @@ interface TabsComponentProps {
 const TabsComponent = ({ rateio }: TabsComponentProps) => {
   return (
     <Tabs defaultValue="expenses" className="w-full">
-      <TabsList className="grid w-full grid-cols-3 max-w-[700px] mb-4">
-        <TabsTrigger value="expenses" className="text-xs md:text-base">Expenses Overview</TabsTrigger>
-        <TabsTrigger value="participants" className="text-xs md:text-base">Participants Overview</TabsTrigger>
-        <TabsTrigger value="suggestions" className="text-xs md:text-base">Suggestions</TabsTrigger>
+      <TabsList className="grid w-full grid-cols-3 max-w-[700px] mb-4 p-0">
+        <TabsTrigger
+          value="expenses"
+          className="text-xs md:text-base flex gap-x-2 items-center"
+        >
+          <BarChartBigIcon /> Expenses
+        </TabsTrigger>
+        <TabsTrigger
+          value="participants"
+          className="text-xs md:text-base flex gap-x-2 items-center"
+        >
+          <UsersIcon /> Participants
+        </TabsTrigger>
+        <TabsTrigger
+          value="suggestions"
+          className="text-xs md:text-base flex gap-x-2 items-center"
+        >
+          <FileHeartIcon /> Suggestions
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="expenses">
         <DashboardTabExpense
